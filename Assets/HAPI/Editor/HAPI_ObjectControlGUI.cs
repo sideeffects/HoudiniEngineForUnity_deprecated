9fe2c72c9d69434c6906027d6110395555cfe547
Damian Campeanu <damian@sidefx.com>
2013-12-04 13:14:56 -0500

    Explicitly adding the default Inspector GUI for object, geo, and part controls so we can mark the shown values as readonly (disabled). Otherwise, people can shoot themselves in the foot by manipulating internal values.

---
+++ Assets/HAPI/Editor/HAPI_ObjectControlGUI.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * PROPRIETARY INFORMATION.  This software is proprietary to
+ * Side Effects Software Inc., and is not to be reproduced,
+ * transmitted, or disclosed in any way without written permission.
+ *
+ * Produced by:
+ *      Side Effects Software Inc
+ *		123 Front Street West, Suite 1401
+ *		Toronto, Ontario
+ *		Canada   M5J 2M2
+ *		416-504-9876
+ *
+ * COMMENTS:
+ * 
+ */
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+using HAPI;
+
+[ CustomEditor( typeof( HAPI_ObjectControl ) ) ]
+[ CanEditMultipleObjects ]
+public class HAPI_ObjectControlGUI : Editor 
+{
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Public
+
+	public void OnSceneGUI()
+	{
+
+	}
+
+	public virtual void OnEnable()
+	{
+
+	}
+
+	public override void OnInspectorGUI()
+	{
+		HAPI_GUI.help( "Values here are for debugging only and should not be modified directly.", MessageType.Info );
+
+		bool gui_enabled = GUI.enabled;
+		GUI.enabled = false;
+		DrawDefaultInspector();
+		GUI.enabled = gui_enabled;
+	}
+}
