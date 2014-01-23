9edaa159bf43bb9563b3073187a11916a505d9f2
Ken Xu <ken@sidefx.com>
2014-01-23 17:09:15 -0500

    fix bug where pinning wouldn't work if the user object being provided was a prefab.

--- Assets/HAPI/Scripts/HAPI_InstancerOverrideInfo.cs
+++ Assets/HAPI/Scripts/HAPI_InstancerOverrideInfo.cs
@@ -8,7 +8,7 @@ public class HAPI_InstancerOverrideInfo : ScriptableObject
 	public Vector3 translate = new Vector3( 0, 0, 0 );
 	public Vector3 rotate = new Vector3( 0, 0, 0 );
 	public Vector3 scale = new Vector3( 0, 0, 0 );
-	public string objectToInstantiatePath;
+	public GameObject objectToInstantiate = null;
 	public int instancePointNumber = -1;
 }
 
