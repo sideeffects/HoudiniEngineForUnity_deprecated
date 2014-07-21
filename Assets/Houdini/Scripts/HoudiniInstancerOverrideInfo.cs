9ef40b85c68422c0f13a020a0a7a188ad1630cd9
Ken Xu <ken@sidefx.com>
2014-07-21 16:05:52 -0400

    Fix for instancing pinning saving not working for objects contained in the asset.

--- Assets/Houdini/Scripts/HoudiniInstancerOverrideInfo.cs
+++ Assets/Houdini/Scripts/HoudiniInstancerOverrideInfo.cs
@@ -7,6 +7,7 @@ public class HoudiniInstancerOverrideInfo : ScriptableObject
 	public Vector3 rotate = new Vector3( 0, 0, 0 );
 	public Vector3 scale = new Vector3( 0, 0, 0 );
 	public GameObject objectToInstantiate = null;
+	public string objectToInstantiatePath;
 	public int instancePointNumber = -1;
 }
 
