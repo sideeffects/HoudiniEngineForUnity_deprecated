696fffbc751863cfebdd2844302dc0945e83b752
Damian Campeanu <damian@sidefx.com>
2014-08-28 12:03:23 -0400

    Bumping to Houdini Version: 13.0.519 Houdini Engine Version: 1.8 (API: 15)

--- Assets/Houdini/Scripts/HoudiniVersion.cs
+++ Assets/Houdini/Scripts/HoudiniVersion.cs
@@ -28,21 +28,21 @@ public struct HoudiniVersion
 {
 	public const int HOUDINI_MAJOR			= 13;
 	public const int HOUDINI_MINOR			= 0;
-	public const int HOUDINI_BUILD			= 512;
+	public const int HOUDINI_BUILD			= 519;
 
 	public const int ORIG_HOUDINI_MAJOR		= 13;
 	public const int ORIG_HOUDINI_MINOR		= 0;
-	public const int ORIG_HOUDINI_BUILD		= 512;
+	public const int ORIG_HOUDINI_BUILD		= 519;
 
 	public const int HOUDINI_ENGINE_MAJOR	= 1;
 	public const int HOUDINI_ENGINE_MINOR	= 8;
 	
-	public const int HOUDINI_ENGINE_API		= 14;
+	public const int HOUDINI_ENGINE_API		= 15;
 	
 #if UNITY_STANDALONE_WIN
 	public const string HAPI_LIBRARY		= "libHAPI";
 #elif UNITY_STANDALONE_OSX
-	public const string HAPI_LIBRARY		= "/Library/Frameworks/Houdini.framework/Versions/13.0.512/Houdini";
+	public const string HAPI_LIBRARY		= "/Library/Frameworks/Houdini.framework/Versions/13.0.519/Houdini";
 #else
 	#error Unsupported platform!
 #endif
