dfc489164d5b06bbc90b57973d48ac78a7671849
Damian Campeanu <damian@sidefx.com>
2014-10-24 19:32:01 -0400

    Fixed dll not found on startup when Windows Apps is set as platform.

--- Assets/Houdini/Scripts/HoudiniSetPath.cs
+++ Assets/Houdini/Scripts/HoudiniSetPath.cs
@@ -20,7 +20,7 @@ public class HoudiniSetPath
 	public static string getHoudiniPath()
 	{
 		string houdini_app_path = "";
-#if UNITY_STANDALONE_WIN
+#if UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR )
 		string hapi_path = System.Environment.GetEnvironmentVariable( "HAPI_PATH", 
 																		System.EnvironmentVariableTarget.Machine );
 		if ( hapi_path == null || hapi_path.Length == 0 )
@@ -62,7 +62,7 @@ public class HoudiniSetPath
 				break;
 			}
 		}
-#endif // UNITY_STANDALONE_WIN
+#endif // UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR )
 
 		return houdini_app_path;
 	}
@@ -74,7 +74,7 @@ public class HoudiniSetPath
 			
 		try
 		{
-#if UNITY_STANDALONE_WIN
+#if UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR )
 			string houdini_app_path = getHoudiniPath();
 			string houdini_bin_path = houdini_app_path + "/bin";
 
@@ -94,7 +94,7 @@ public class HoudiniSetPath
 			myIsPathSet = true;
 #else
 			myIsPathSet = true;
-#endif // UNITY_STANDALONE_WIN
+#endif // UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR )
 		}
 		catch ( HoudiniError error )
 		{
@@ -114,7 +114,7 @@ public class HoudiniSetPath
 			return app_path;
 #endif // UNITY_EDITOR
 
-#if ( UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || ( UNITY_METRO && UNITY_EDITOR ) )
+#if ( UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR ) )
 		// For Windows, we look at the registry entries made by the Houdini installer. We look for the 
 		// "active version" key which gives us the most recently installed Houdini version. Using the
 		// active version we find the registry made by that particular installer and find the install
@@ -147,7 +147,7 @@ public class HoudiniSetPath
 		//#error "Your current platform is not yet fully supported. Binaries search path not set."
 		Debug.LogError( "Your current platform is not yet full support. Binaries search path not set." );
 
-#endif // ( UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || ( UNITY_METRO && UNITY_EDITOR ) )
+#endif // ( UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR ) )
 
 		return app_path;
 	}
