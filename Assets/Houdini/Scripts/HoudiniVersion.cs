837fd825e0af32790ce22f088b5c76a53c4c9eed
Damian Campeanu <damian@sidefx.com>
2014-10-24 10:05:56 -0400

    Bumping to Houdini Version: 13.0.575 Houdini Engine Version: 1.8 (API: 18)

--- Assets/Houdini/Scripts/HoudiniVersion.cs
+++ Assets/Houdini/Scripts/HoudiniVersion.cs
@@ -28,22 +28,22 @@ public struct HoudiniVersion
 {
 	public const int HOUDINI_MAJOR			= 13;
 	public const int HOUDINI_MINOR			= 0;
-	public const int HOUDINI_BUILD			= 568;
+	public const int HOUDINI_BUILD			= 575;
 
 	public const int ORIG_HOUDINI_MAJOR		= 13;
 	public const int ORIG_HOUDINI_MINOR		= 0;
-	public const int ORIG_HOUDINI_BUILD		= 568;
+	public const int ORIG_HOUDINI_BUILD		= 575;
 
 	public const int HOUDINI_ENGINE_MAJOR	= 1;
 	public const int HOUDINI_ENGINE_MINOR	= 8;
 	
 	public const int HOUDINI_ENGINE_API		= 18;
 	
-#if UNITY_STANDALONE_WIN
+#if UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR )
 	public const string HAPI_LIBRARY		= "libHAPI";
 #elif UNITY_STANDALONE_OSX
-	public const string HAPI_LIBRARY		= "/Library/Frameworks/Houdini.framework/Versions/13.0.568/Houdini";
+	public const string HAPI_LIBRARY		= "/Library/Frameworks/Houdini.framework/Versions/13.0.575/Houdini";
 #else
-	public const string HAPI_LIBRARY		= "";
+	public const string HAPI_LIBRARY		= "libHAPI"; // Cannot be empty but its ok if not found.
 #endif
 };
