2dff900210055e96bcb3cd1528bb67b28c1ec2ec
Damian Campeanu <damian@sidefx.com>
2014-11-24 21:07:45 -0500

    Added temporary support for Unity 4.3.

--- Assets/Houdini/Editor/HoudiniCurveGUI.cs
+++ Assets/Houdini/Editor/HoudiniCurveGUI.cs
@@ -465,11 +465,11 @@ public class HoudiniCurveGUI : Editor
 			if ( mySelectionMaterial.SetPass( 0 ) )
 			{
 				// TODO: Clean this up!
-#if UNITY_4_5 || UNITY_4_6
+#if UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6
 				if ( PlayerSettings.useDirect3D11 )
 #else
 				if ( PlayerSettings.graphicsAPI == GraphicsAPI.Direct3D11 )
-#endif
+#endif // UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6
 				{
 					Camera tempCamera = Camera.current;
 					float s = HoudiniHost.prGuidePointSize / 2.0f;
@@ -517,11 +517,11 @@ public class HoudiniCurveGUI : Editor
 			if ( mySelectionMaterial.SetPass( 1 ) )
 			{
 				// TODO: Clean this up!
-#if UNITY_4_5 || UNITY_4_6
+#if UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6
 				if ( PlayerSettings.useDirect3D11 )
 #else
 				if ( PlayerSettings.graphicsAPI == GraphicsAPI.Direct3D11 )
-#endif
+#endif // UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6
 				{
 					Camera tempCamera = Camera.current;
 					float s = ( HoudiniHost.prGuidePointSize - myGuideBorderSize ) / 2.0f;
