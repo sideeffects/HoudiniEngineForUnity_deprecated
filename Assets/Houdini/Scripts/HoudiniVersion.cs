45523788893085bca78cb71215c10e17842aac2c
Damian Campeanu <damian@sidefx.com>
2015-04-20 17:45:21 -0400

    Bumping to Houdini Version: 14.5.89 Houdini Engine Version: 1.9 (API: 19)

--- Assets/Houdini/Scripts/HoudiniVersion.cs
+++ Assets/Houdini/Scripts/HoudiniVersion.cs
@@ -39,7 +39,7 @@ public struct HoudiniVersion
 	public const int HOUDINI_ENGINE_MAJOR	= 1;
 	public const int HOUDINI_ENGINE_MINOR	= 9;
 	
-	public const int HOUDINI_ENGINE_API		= 18;
+	public const int HOUDINI_ENGINE_API		= 19;
 	
 #if UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR )
 	public const string HAPI_LIBRARY		= "libHAPI";
