9c0a24c8618b682de845a1da18d75cd24bb70ce2
Damian Campeanu <damian@sidefx.com>
2015-07-31 16:50:30 -0400

    Bumping to Houdini Version: 15.0.190 Houdini Engine Version: 2.0 (API: 5)

--- Assets/Houdini/Scripts/HoudiniVersion.cs
+++ Assets/Houdini/Scripts/HoudiniVersion.cs
@@ -26,20 +26,20 @@
 
 public struct HoudiniVersion
 {
-	public const int HOUDINI_MAJOR			= 14;
-	public const int HOUDINI_MINOR			= 5;
-	public const int HOUDINI_BUILD			= 152;
+	public const int HOUDINI_MAJOR			= 15;
+	public const int HOUDINI_MINOR			= 0;
+	public const int HOUDINI_BUILD			= 190;
 	public const int HOUDINI_PATCH			= 0;
 
 	public const int HOUDINI_ENGINE_MAJOR	= 2;
 	public const int HOUDINI_ENGINE_MINOR	= 0;
 	
-	public const int HOUDINI_ENGINE_API		= 4;
+	public const int HOUDINI_ENGINE_API		= 5;
 	
 #if UNITY_STANDALONE_WIN || ( UNITY_METRO && UNITY_EDITOR )
 	public const string HAPI_LIBRARY		= "libHAPI";
 #elif UNITY_STANDALONE_OSX
-	public const string HAPI_LIBRARY		= "/Library/Frameworks/Houdini.framework/Versions/14.5.152/Houdini";
+	public const string HAPI_LIBRARY		= "/Library/Frameworks/Houdini.framework/Versions/15.0.190/Houdini";
 #else
 	public const string HAPI_LIBRARY		= "libHAPI"; // Cannot be empty but its ok if not found.
 #endif
