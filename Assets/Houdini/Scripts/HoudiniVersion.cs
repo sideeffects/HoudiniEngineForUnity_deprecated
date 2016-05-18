6a11c22b78d02f66750389ffcec841dc3f378153
Damian Campeanu <damian@sidefx.com>
2016-05-18 15:03:38 -0400

    Bumping to Houdini Version: 15.0.481 Houdini Engine Version: 2.0 (API: 23)

--- Assets/Houdini/Scripts/HoudiniVersion.cs
+++ Assets/Houdini/Scripts/HoudiniVersion.cs
@@ -28,7 +28,7 @@ public struct HoudiniVersion
 {
 	public const int HOUDINI_MAJOR			= 15;
 	public const int HOUDINI_MINOR			= 0;
-	public const int HOUDINI_BUILD			= 458;
+	public const int HOUDINI_BUILD			= 481;
 	public const int HOUDINI_PATCH			= 0;
 
 	public const int HOUDINI_ENGINE_MAJOR	= 2;
@@ -47,11 +47,11 @@ public struct HoudiniVersion
 
 #elif UNITY_STANDALONE_OSX
 
-	public const string HAPI_SERVER			= "/Library/Frameworks/Houdini.framework/Versions/15.0.458/Resources/bin/" + "HARS";
+	public const string HAPI_SERVER			= "/Library/Frameworks/Houdini.framework/Versions/15.0.481/Resources/bin/" + "HARS";
 	#if UNITY_EDITOR_64
-		public const string HAPI_LIBRARY	= "/Library/Frameworks/Houdini.framework/Versions/15.0.458/Libraries/" + "libHARC.dylib";
+		public const string HAPI_LIBRARY	= "/Library/Frameworks/Houdini.framework/Versions/15.0.481/Libraries/" + "libHARC.dylib";
 	#else
-		public const string HAPI_LIBRARY	= "/Library/Frameworks/Houdini.framework/Versions/15.0.458/Libraries/" + "libHARC32.dylib";
+		public const string HAPI_LIBRARY	= "/Library/Frameworks/Houdini.framework/Versions/15.0.481/Libraries/" + "libHARC32.dylib";
 	#endif // UNITY_EDITOR_64
 
 #else
