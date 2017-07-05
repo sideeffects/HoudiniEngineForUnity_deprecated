f819002197cc1b63600475b96e1319ba381c604a
Seelan Vamatheva <seelanv@sidefx.com>
2017-07-05 16:03:30 -0400

    Bumping to Houdini Version: 16.5.164 Houdini Engine Version: 3.1 (API: 5)

--- Assets/Houdini/Scripts/HoudiniVersion.cs
+++ Assets/Houdini/Scripts/HoudiniVersion.cs
@@ -34,7 +34,7 @@ public struct HoudiniVersion
 	public const int HOUDINI_ENGINE_MAJOR	= 3;
 	public const int HOUDINI_ENGINE_MINOR	= 1;
 
-	public const int HOUDINI_ENGINE_API		= 4;
+	public const int HOUDINI_ENGINE_API		= 5;
 
 #if UNITY_EDITOR_WIN || ( UNITY_METRO && UNITY_EDITOR )
 
