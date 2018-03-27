3eb6baa0555bf51c86eef8fdd18aeddfc8d24f19
Seelan Vamatheva <seelanv@sidefx.com>
2018-03-27 09:41:37 -0400

    Fix for MarkSceneDirty error in playmode for version 1.

--- Assets/Houdini/Scripts/HoudiniAsset.cs
+++ Assets/Houdini/Scripts/HoudiniAsset.cs
@@ -1266,11 +1266,14 @@ public abstract class HoudiniAsset : HoudiniObjectControl
 #endif // !( HAPI_ENABLE_RUNTIME )
 
 #if UNITY_EDITOR && UNITY_5_3_OR_NEWER
-		// Mark the scene dirty after we do a build to make sure the changes get saved
-		UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene());
+		if (Application.isEditor && !Application.isPlaying)
+		{
+			// Mark the scene dirty after we do a build to make sure the changes get saved
+			UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene());
+		}
 #endif
 
-		return true;
+			return true;
 	}
 	
 	public void updateParameters( HoudiniProgressBar progress_bar )
